631a489e606dfc70688181192d538d8bd9fc841d
eyeol <42065198+eyeol@users.noreply.github.com>
2024-10-18 13:42:18 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,15 +1,12 @@
 def solution(num_list):
-    answer = 0
     
-    sum_add = 0
     sum_mul = 1
     
     for num in num_list:
-        sum_add += num
         sum_mul *= num
     
-    if sum_mul < sum_add**2:
-        answer = 1
+    if sum_mul < sum(num_list)**2:
+        return 1
     
-    return answer
+    return 0
 
