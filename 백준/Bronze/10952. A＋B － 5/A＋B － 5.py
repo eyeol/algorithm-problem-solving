c0ed6370e742c01ebb29071a31a1468c3e6e5648
eyeol <42065198+eyeol@users.noreply.github.com>
2024-10-18 17:17:46 +0900

    [Bronze V] Title: A+B - 5, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,5 +1,2 @@
-while True:
-    a, b = map(int, input().split())
-    if a == 0 and b == 0:
-        break
-    print(a + b)
+while (pair := list(map(int, input().split()))) != [0, 0]:
+    print(sum(pair))
