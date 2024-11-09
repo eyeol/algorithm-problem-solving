b6e68bd817149fe57f208234ded80d7fb8daf9e1
eyeol <42065198+eyeol@users.noreply.github.com>
2024-11-09 18:22:23 +0900

    [Silver III] Title: 칸토어 집합, Time: 92 ms, Memory: 111860 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,32 +1,18 @@
-def divide_and_remove(kant, start, end):
-    if (end - start) == 1:
-        return
-    else:
-        length = end - start
-        checkpoint_1 = start + length // 3
-        checkpoint_2 = start + (length // 3) * 2
-
-        for i in range(checkpoint_1, checkpoint_2):
-            kant[i] = " "
-
-        divide_and_remove(kant, start, checkpoint_1)
-        divide_and_remove(kant, checkpoint_2, end)
-
-
 import sys
+
+
+def cantor(N):
+    if N == 0:
+        return "-"
+    child = cantor(N - 1)
+
+    return child + " " * (3 ** (N - 1)) + child
 
 
 def sol():
     for line in sys.stdin:
         N = int(line.strip())
-        kant = ["-"] * (3**N)
-        start = 0
-        end = len(kant)
-
-        divide_and_remove(kant, start, end)
-
-        result = "".join(kant)
-        print(result)
+        print(cantor(N))
 
 
 sol()
