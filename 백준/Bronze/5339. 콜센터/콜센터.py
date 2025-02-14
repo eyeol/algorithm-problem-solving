555801bbc489ba67965832a8242e69a78f7b2b3c
eyeol <42065198+eyeol@users.noreply.github.com>
2025-02-15 01:57:24 +0900

    [Bronze V] Title: 콜센터, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+
+    print("     /~\\")
+    print("    ( oo|")
+    print("    _\\=/_")
+    print("   /  _  \\")
+    print("  //|/.\\|\\\\")
+    print(" ||  \\ /  ||")
+    print("============")
+    print("|          |")
+    print("|          |")
+    print("|          |")
+
+
+solution()
