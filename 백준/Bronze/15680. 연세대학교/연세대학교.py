38c19825be548f62742d622b6de47c015e269d74
eyeol <42065198+eyeol@users.noreply.github.com>
2025-03-16 22:48:04 +0900

    [Bronze V] Title: 연세대학교, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline  # 빠른 입력
+
+
+def solution():
+    N = int(input())
+    if N == 0:
+        print("YONSEI")
+    else:
+        print("Leading the Way to the Future")
+
+
+solution()
