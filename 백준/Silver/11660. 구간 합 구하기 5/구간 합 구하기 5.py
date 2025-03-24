e538b412e5367f0a159f8db2a9d14a78d6c8058e
eyeol <42065198+eyeol@users.noreply.github.com>
2025-03-24 15:10:38 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 312 ms, Memory: 129716 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -4,28 +4,45 @@ input = sys.stdin.readline
 
 
 def solution():
+    # N X N 행렬, M : testcase 수
     N, M = map(int, input().split())
+
     board = []
-    sum_board = []
     for _ in range(N):
-        # 일반 배열
         arr = list(map(int, input().split()))
-        # 합 배열
-        sum_arr = []
-        summation = 0
-        for num in arr:
-            summation += num
-            sum_arr.append(summation)
         board.append(arr)
-        sum_board.append(sum_arr)
-    for i in range(M):
-        result = 0
+
+    sum_board = [[0] * N for _ in range(N)]
+    summation_x = 0
+    summation_y = 0
+    for i in range(N):
+        summation_x += board[0][i]
+        summation_y += board[i][0]
+        sum_board[0][i] = summation_x
+        sum_board[i][0] = summation_y
+
+    for i in range(1, N):
+        for j in range(1, N):
+            sum_board[i][j] = (
+                sum_board[i][j - 1]
+                + sum_board[i - 1][j]
+                - sum_board[i - 1][j - 1]
+                + board[i][j]
+            )
+
+    for _ in range(M):
         x1, y1, x2, y2 = map(int, input().split())
-        for j in range(x1 - 1, x2):  # x1-1에서 x2-1까지
-            if y1 > 1:
-                result += sum_board[j][y2 - 1] - sum_board[j][y1 - 2]
-            else:
-                result += sum_board[j][y2 - 1]
+        x1 -= 1
+        y1 -= 1
+        x2 -= 1
+        y2 -= 1
+        result = sum_board[x2][y2]
+        if x1 > 0:
+            result -= sum_board[x1 - 1][y2]
+        if y1 > 0:
+            result -= sum_board[x2][y1 - 1]
+        if x1 > 0 and y1 > 0:
+            result += sum_board[x1 - 1][y1 - 1]
         print(result)
 
 
