958e1dadf10468fdf80ab6762e0fde91e0d8337b
eyeol <42065198+eyeol@users.noreply.github.com>
2025-03-24 15:47:59 +0900

    [Gold III] Title: 나머지 합, Time: 368 ms, Memory: 258100 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -7,22 +7,17 @@ def solution():
     N, M = map(int, input().split())
     A = list(map(int, input().split()))
 
-    # P = []
     remain = [0] * M
     prefix = 0
     for number in A:
         prefix += number
-        # P.append(prefix)
         remain[prefix % M] += 1
 
-    count = 0
+    ans = remain[0]
     for i in range(M):
-        candidate = remain[i]
-        satisfied = candidate * (candidate - 1) / 2
-        count += int(satisfied)
-    count += remain[0]
+        ans += (remain[i] * (remain[i] - 1)) // 2
 
-    print(count)
+    print(ans)
 
 
 solution()
