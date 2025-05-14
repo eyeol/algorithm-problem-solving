6bab4ad97889adb6e65253aaa5d73d5b464c5480
eyeol <42065198+eyeol@users.noreply.github.com>
2025-05-14 17:31:21 +0900

    [Silver II] Title: 스택 수열, Time: 132 ms, Memory: 117772 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,54 +1,27 @@
 import sys
 
-input = sys.stdin.readline
-
-
 def solution():
+    input = sys.stdin.readline
     N = int(input())
-
     stack = []
-    count = 1
+    result = []
+    current = 1
 
-    push_pop = []
-
-    flag = True
-
-    # 숫자가 하나 들어왔을 떄 push pop 표현해야 함
     for _ in range(N):
-        number = int(input())
+        target = int(input())
 
-        # count가 number에 도달해야 함
-        while count <= number:
-            stack.append(count)
-            count += 1
-            # push
-            push_pop.append("+")
+        while current <= target:
+            stack.append(current)
+            result.append("+")
+            current += 1
 
-        # 마지막 숫자
-        if stack:
-            pop_num = stack[-1]
-            if pop_num < number and count > number:
-                flag = False
+        if stack and stack[-1] == target:
+            stack.pop()
+            result.append("-")
+        else:
+            print("NO")
+            return
 
-            while stack and pop_num >= number:
-
-                stack.pop()
-                # pop
-                push_pop.append("-")
-
-                # pop_num 최신화
-                if stack:
-                    pop_num = stack[-1]
-
-        else:  # 스택이 빈 경우
-            if count > number:
-                flag = False
-
-    if flag:
-        for operand in push_pop:
-            print(operand)
-    else:
-        print("NO")
-
+    print('\n'.join(result))
 
 solution()
