5846849fa138c9bfbcc828cdfce8c95d38c04847
eyeol <42065198+eyeol@users.noreply.github.com>
2025-05-20 17:00:42 +0900

    [Silver II] Title: DFS와 BFS, Time: 136 ms, Memory: 115116 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -3,22 +3,18 @@ from collections import deque
 
 input = sys.stdin.readline
 
-# 일단 인접 행렬로 풀어보고
-# 그 다음 인접 리스트로 풀자
-
 dfs_result = []
 bfs_result = []
 
 
 def dfs(node: int):
-    if not visited_dfs[node]:
-        visited_dfs[node] = 1
-        dfs_result.append(node)
-        for i, connected in enumerate(adj[node]):
-            if connected:
-                dfs(i)
-    else:
+    if visited[node]:
         return
+
+    visited[node] = 1
+    dfs_result.append(node)
+    for i in adj[node]:
+        dfs(i)
 
 
 def bfs(node: int):
@@ -26,29 +22,31 @@ def bfs(node: int):
 
     while deq:
         out = deq.popleft()
-        if not visited_bfs[out]:
-            visited_bfs[out] = 1
+        if not visited[out]:
+            visited[out] = 1
             bfs_result.append(out)
-            for i, connected in enumerate(adj[out]):
-                if connected:
+            for i in adj[out]:
+                if not visited[i]:
                     deq.append(i)
 
 
 def solution():
     N, M, V = map(int, input().split())
     global adj
-    adj = [[0] * (N + 1) for _ in range(N + 1)]
-    global visited_dfs
-    global visited_bfs
-    visited_dfs = [0] * (N + 1)
-    visited_bfs = [0] * (N + 1)
+    adj = [[] for _ in range(N + 1)]
+
+    global visited
+    visited = [0] * (N + 1)
     for _ in range(M):
         i, j = map(int, input().split())
         # undirected graph
-        adj[i][j] = 1
-        adj[j][i] = 1
+        adj[i].append(j)
+        adj[j].append(i)
+    for i in range(1, N + 1):
+        adj[i].sort()
 
     dfs(V)
+    visited = [0] * (N + 1)
     bfs(V)
     print(" ".join(map(str, dfs_result)))
     print(" ".join(map(str, bfs_result)))
