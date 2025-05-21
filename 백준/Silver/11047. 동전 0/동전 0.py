919b206c2b2269349db7dfb5b32d8a07db95c0db
eyeol <42065198+eyeol@users.noreply.github.com>
2025-05-21 22:03:35 +0900

    [Silver IV] Title: 동전 0, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,14 +1,9 @@
-import sys
-
-input = sys.stdin.readline
-
-
 def solution():
     N, K = map(int, input().split())
     coins = [int(input()) for _ in range(N)]
 
     count = 0
-    for coin in coins[::-1]:
+    for coin in reversed(coins):
         count += K // coin
         K = K % coin
 
