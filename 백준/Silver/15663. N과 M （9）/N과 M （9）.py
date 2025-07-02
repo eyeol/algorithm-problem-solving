1cfed32867267fc8ad5f003202635be7efba2cfe
eyeol <42065198+eyeol@users.noreply.github.com>
2025-07-02 17:25:41 +0900

    [Silver II] Title: N과 M (9), Time: 140 ms, Memory: 114512 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -8,8 +8,7 @@ def solution():
     N, M = map(int, input().split())
     a = list(map(int, input().split()))
 
-    result = list(permutations(a, M))
-    result = list(set(result))
+    result = list(set(permutations(a, M)))
     result.sort()
     for comb in result:
         print(" ".join(map(str, comb)))
