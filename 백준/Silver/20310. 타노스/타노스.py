ce41fa70e51a545d349eb7f511c34f7e0fec9537
eyeol <42065198+eyeol@users.noreply.github.com>
2025-08-06 22:38:24 +0900

    [Silver III] Title: 타노스, Time: 88 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -6,22 +6,28 @@ input = sys.stdin.readline
 def solution():
     s = input().strip()
 
-    zeros = []
     ones = []
+    zeros = []
 
-    for ch in s:
+    for i, ch in enumerate(s):
         num = int(ch)
         if num == 1:
-            ones.append(num)
+            ones.append((1, i))
         else:  # num = 0
-            zeros.append(num)
+            zeros.append((0, i))
 
+    ones = ones[len(ones) // 2 :]
     zeros = zeros[: len(zeros) // 2]
-    ones = ones[: len(ones) // 2]
 
-    result = sorted(zeros + ones)
+    result = ones + zeros
 
-    print("".join(map(str, result)))
+    result.sort(key=lambda x: x[1])
+
+    out = []
+    for re in result:
+        out.append(re[0])
+
+    print("".join(map(str, out)))
 
 
 if __name__ == "__main__":
