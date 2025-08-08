deb84e1b9e75ce0a72aa5a224f9ccc6f5b45dc12
eyeol <42065198+eyeol@users.noreply.github.com>
2025-08-08 21:57:17 +0900

    [Gold V] Title: 회의실 배정, Time: 172 ms, Memory: 142248 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -9,18 +9,18 @@ it = iter(data[1:])
 for s, e in zip(it, it):
     buckets[int(s)].append(int(e))
 
-keys = sorted(buckets)           # int 비교
+keys = sorted(buckets)           
 for k in keys:
-    buckets[k].sort()            # int 비교
+    buckets[k].sort()            
 
 cur_end = -1
 cnt = 0
 for k in keys:
     for e in buckets[k]:
-        if cur_end <= k:         # 겹치지 않으면 회의 선택
+        if cur_end <= k:         
             cnt += 1
-            cur_end = e          # 종료 시각 갱신
-        else:                    # 겹치면 더 빨리 끝나는 회의로 교체
+            cur_end = e          
+        else:                    
             cur_end = min(cur_end, e)
 
 print(cnt)
