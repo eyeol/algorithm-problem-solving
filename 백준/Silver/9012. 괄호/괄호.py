ea637fd20fbdb18185e93df9445bb694578fcf66
eyeol <42065198+eyeol@users.noreply.github.com>
2025-08-10 07:10:43 +0900

    [Silver IV] Title: 괄호, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -2,28 +2,16 @@ import sys
 
 input = sys.stdin.readline
 
-# Given
-# Valid PS 정보
-
-# Goal
-# 주어진 문자열에 대해 VPS인지 아닌지 determine
-
-# How to solve
-# 이건 스택/큐/힙 문제인데 어떤걸 사용해야 하는지 생각해보자
-
-
-def determine(ps: str):
-    cnt = 0
+def is_vps(ps: str):
+    stack = []
     for ch in ps:
         if ch == "(":
-            cnt += 1
+            stack.append(ch)
         else:
-            cnt -= 1
-        if cnt < 0:
-            return "NO"
-    if cnt != 0:
-        return "NO"
-    return "YES"
+            if not stack:
+                return "NO"
+            stack.pop()
+    return "YES" if not stack else "NO"
 
 
 def solution():
@@ -32,7 +20,7 @@ def solution():
     for _ in range(T):
         ps = input().strip()
 
-        print(determine(ps))
+        print(is_vps(ps))
 
 
 if __name__ == "__main__":
