d254263105c165575b7afcd76e6b03932da26a5f
eyeol <42065198+eyeol@users.noreply.github.com>
2025-08-18 20:49:09 +0900

    [Silver IV] Title: 제로, Time: 128 ms, Memory: 115832 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,12 +1,30 @@
-K = int(input())
+import sys
+from collections import deque
 
-money_book = []
+input = sys.stdin.readline
 
-for i in range(K):
-    num = int(input().strip())
-    if num == 0:
-        money_book.pop(-1)
-    else:
-        money_book.append(num)
+# Given
+# 재현이가 잘못된 수를 부르면 0을 외쳐서 쓴 수를 지우게 함
 
-print(sum(money_book))
+# Goal
+# 모든 수 받아적은 후 합을 알고 싶음
+
+# How to solve
+# 가장 최근에 쓴 수를 지우는거라서 후입선출 = 스택 사용
+
+
+def solution():
+    K = int(input())
+    stacks = []
+    for _ in range(K):
+        x = int(input())
+        if x == 0:
+            stacks.pop()
+        else:
+            stacks.append(x)
+
+    print(sum(stacks))
+
+
+if __name__ == "__main__":
+    solution()
