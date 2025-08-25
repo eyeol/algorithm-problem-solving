9743d1d5e8fa996f1806b3be782b74c2b84b094f
eyeol <42065198+eyeol@users.noreply.github.com>
2025-08-25 21:21:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -7,11 +7,19 @@ dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for i in range(4, 11):
+for i in range(4, 11):  # 4부터 10까지
     dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
 
-T = int(input())
 
-for _ in range(T):
-    n = int(input())
-    print(dp[n])
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        if N <= 0:
+            print(0)
+        else:
+            print(dp[N])
+
+
+if __name__ == "__main__":
+    main()
