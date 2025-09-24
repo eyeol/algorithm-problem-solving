48fdff31e9fbc63056c54e513cc68ac8403c6981
eyeol <42065198+eyeol@users.noreply.github.com>
2025-09-24 22:10:58 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 260 ms, Memory: 118608 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.py
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.py
@@ -1,57 +1,37 @@
 import sys
-
 input = sys.stdin.readline
 
-# 1000을 1000번 더한 값보다 크도록
-INF = 1000_000_000
+INF = 10**15
 
 def solution():
     N, M = map(int, input().split())
 
-    space = []
-    # N x M 행렬 생성
-    for _ in range(N):
-        space.append(list(map(int, input().split())))
-    
-    # 동일한 사이즈의 dp table
-    dp = [[[INF, INF, INF] for _ in range(M)] for _ in range(N)]
+    row = list(map(int, input().split()))
+    prev = [[row[j]] * 3 for j in range(M)] 
 
-    for j in range(M):
-        for i, v in enumerate(dp[0][j]):
-            dp[0][j][i] = space[0][j]
-    
+    for _ in range(1, N):
+        row = list(map(int, input().split()))
+        curr = [[INF, INF, INF] for _ in range(M)]
 
-    for i in range(1, N): # 1부터 N-1
-        for j in range(M): # 0부터 M-1
-            prev = dp[i-1]
-            left = -1
-            right = M
-            
-            if j-1 >= 0:
-                left = j-1
-            if j+1 < M:
-                right = j+1
+        last = M - 1
+        for j in range(M):
+            v = row[j]
 
-            # left가 범위에 있으면
-            if left >= 0:
-                lmin_fuel = min(prev[left][1], prev[left][2])
-                dp[i][j][0] = space[i][j] + lmin_fuel
-            
-            # right가 범위에 있으면
-            if right < M:
-                rmin_fuel = min(prev[right][0], prev[right][1])
-                dp[i][j][2] = space[i][j] + rmin_fuel
-            
-            cmin_fuel = min(prev[j][0], prev[j][2])
-            dp[i][j][1] = space[i][j] + cmin_fuel
-    
-    minimum = INF
-    for j in range(M):
-        for k in range(3):
-            if dp[N-1][j][k] < minimum:
-                minimum = dp[N-1][j][k]
-    
-    print(minimum)
+            if j > 0:
+                t = prev[j - 1]
+                curr[j][0] = v + (t[1] if t[1] < t[2] else t[2])
+
+            t = prev[j]
+            curr[j][1] = v + (t[0] if t[0] < t[2] else t[2])
+
+            if j < last:
+                t = prev[j + 1]
+                curr[j][2] = v + (t[0] if t[0] < t[1] else t[1])
+
+        prev = curr
+
+    ans = min(min(cell) for cell in prev)
+    print(ans)
 
 if __name__ == "__main__":
     solution()
