dafd93cbe7e0900ea7614593bfd2f8eaa1fae660
eyeol <42065198+eyeol@users.noreply.github.com>
2025-09-28 21:07:51 +0900

    [Gold IV] Title: LCS 3, Time: 132 ms, Memory: 111112 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -1,7 +1,5 @@
 import sys
-
 input = sys.stdin.readline
-
 
 def solution():
     a = input().strip()
@@ -10,17 +8,23 @@ def solution():
 
     A, B, C = len(a), len(b), len(c)
 
-    dp = [[[0] * (C + 1) for _ in range(B + 1)] for __ in range(A + 1)]
+    prev = [[0] * (C + 1) for _ in range(B + 1)]
+    curr = [[0] * (C + 1) for _ in range(B + 1)]
 
-    for i in range(1, A+1):
-        for j in range(1, B+1):
-            for k in range(1, C+1):
+    for i in range(1, A + 1):
+        for j in range(1, B + 1):
+            for k in range(1, C + 1):
                 if a[i-1] == b[j-1] == c[k-1]:
-                    dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+                    curr[j][k] = prev[j-1][k-1] + 1
                 else:
-                    dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+                    curr[j][k] = max(
+                        prev[j][k],   
+                        curr[j-1][k], 
+                        curr[j][k-1]  
+                    )
+        prev, curr = curr, prev 
 
-    print(dp[A][B][C])
+    print(prev[B][C])
 
 if __name__ == "__main__":
     solution()
