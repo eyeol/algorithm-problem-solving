000f3e5114bc11022b04fff38631300d87fdedfe
eyeol <42065198+eyeol@users.noreply.github.com>
2025-10-26 17:17:23 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    while True:
+        name, age, weight = input().split()
+        age, weight = int(age), int(weight)
+        if name == "#" and age == 0 and weight == 0:
+            break
+        if age > 17 or weight >= 80:
+            print(name, "Senior")
+        else:
+            print(name, "Junior")
+
+if __name__ == "__main__":
+    solution()
